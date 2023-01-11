fdbac70489b70bb40897cdb532c45bdca179ef43
reiya <reiya.blogger1495@gmail.com>
2023-01-11 19:37:22 +0900

    コメントアウトを１行追加

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
+    # Flaskインスタンスを作成
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
